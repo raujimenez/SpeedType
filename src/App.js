4fa7146f82b5307e85667fd20482811f3801e0cb
Raul Jimenez <officialrauljimenez@gmail.com>
2020-03-22 12:30:25 -0500

    get random word

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import WordDisplay from "./components/WordDisplay.jsx";
 
+import getWord from "./helpers/getWord.js";
+
 import "./App.css";
 
 function App() {
