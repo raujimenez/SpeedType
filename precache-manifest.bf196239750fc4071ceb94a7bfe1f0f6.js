2a74d8a7083cf8b9b9a3f3502f0cdd6c12842563
Raul Jimenez <officialrauljimenez@gmail.com>
2020-06-14 10:14:40 -0500

    Updates

---
+++ precache-manifest.bf196239750fc4071ceb94a7bfe1f0f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79eba5317c7a176b56f2f6d280fe92c",
+    "url": "/SpeedType/index.html"
+  },
+  {
+    "revision": "53c6110ff6d2e6523380",
+    "url": "/SpeedType/static/css/main.df739979.chunk.css"
+  },
+  {
+    "revision": "c144724c407771a6689e",
+    "url": "/SpeedType/static/js/2.025bf4af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpeedType/static/js/2.025bf4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c6110ff6d2e6523380",
+    "url": "/SpeedType/static/js/main.34d0236f.chunk.js"
+  },
+  {
+    "revision": "6dcec65b6863396580af",
+    "url": "/SpeedType/static/js/runtime-main.4c15f97c.js"
+  }
+]);
